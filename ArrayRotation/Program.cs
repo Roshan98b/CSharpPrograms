be9b3f7af1d30b1ae938c9f5958b0407fdaf6650
Roshan Badrinath <roshan98b@gmail.com>
2021-07-08 19:42:39 +0530

    Add project files.

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+
+        static int gcd(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            } 
+            else
+            {
+                return gcd(b, a % b);
+            }
+        }
+
+        static void arrayRotation(List<int> arr, int size, int dir, int pos)
+        {
+            int gcd_value = gcd(size, pos);
+            for (int i = 0; i < gcd_value; i++)
+            {
+                int temp = arr[i];
+                int j = i;
+                while (true)
+                {
+                    int k = j + pos;
+                    
+                    arr[j] = arr[(j + pos) % size];
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>();
+            int dir, pos, size;
+
+            Console.Write("Enter size of array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter direction for rotation: ");
+            dir = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number of rotations: ");
+            pos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter elements: ");
+            for (int i = 0; i < size; i++)
+            {
+                arr.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            arrayRotation(arr, size, dir, pos);
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static bool IsPalindrome(string word)
+        {
+            for (int i = 0; i < word.Length/2; i++)
+            {
+                if (word[i] != word[word.Length-(i+1)])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader streamReader = new StreamReader(@"C:\Users\robadrin\source\repos\ConsoleApp1\ConsoleApp1\data.txt");
+                List<string> words = new List<string>();
+                int count = 0;
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    words.AddRange(line.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries));
+                }
+                foreach(string word in words)
+                {
+                    if (IsPalindrome(word.ToLower()))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Count = " + count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
