907b2dda5ff4539a39428d06286684c70513c87c
Roshan Badrinath <35145161+Roshan98b@users.noreply.github.com>
2022-06-25 17:59:05 +0530

    Added problems from interview (#7)

---
+++ Problems/PageCompression/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PageCompression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Page> pages = new List<Page>
+            {
+                new Page(1, 5),
+                new Page(4, 6),
+                new Page(10, 5)
+            };
+
+            var result = TransformOverlapOrContiguous(pages);
+
+            foreach (Page page in result)
+            {
+                Console.WriteLine($"start: {page.Start}, length: {page.Length}");
+            }
+        }
+
+        private static List<Page> TransformOverlapOrContiguous(List<Page> pages)
+        {
+            for (int i = 0; i < pages.Count - 1; i++)
+            {
+                if (pages[i].End > pages[i + 1].Start || pages[i].End + 1 == pages[i + 1].Start)
+                {
+                    Page newPage = new Page(pages[i].Start, pages[i].Start + pages[i + 1].End - 1);
+                    pages[i] = newPage;
+                    pages.RemoveAt(i + 1);
+                    i--;
+                }
+            }
+            return pages;
+        }
+
+        internal class Page
+        {
+            public int Start { get; set; }
+            public int Length { get; set; }
+            public int End { get { return this.Start + this.Length - 1; } }
+
+            public Page(int start, int length)
+            {
+                this.Start = start;
+                this.Length = length;
+            }
+
+        }
+    }
+}
---
+++ Problems/SortedSquares/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSquares
+{
+    internal class Program
+    {
+        private static int BinarySearch(int[] nums)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + ((high - low) / 2);
+                if (nums[mid] < 0)
+                {
+                    low = mid + 1;
+                } 
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return low;
+        }
+
+        private static int[] SortedSquares(int[] nums)
+        {
+            List<int> result = new List<int>();
+            int pos = BinarySearch(nums);
+
+            if (pos == nums.Length)
+            {
+                pos--;
+            }
+
+            int i = pos - 1;
+            int j = pos;
+
+            while (i >= 0 && j <= nums.Length - 1)
+            {
+                if (-nums[i] < nums[j])
+                {
+                    result.Add(-nums[i] * -nums[i]);
+                    i--;
+                }
+                else
+                {
+                    result.Add(nums[j] * nums[j]);
+                    j++;
+                }
+            }
+
+            while (i >= 0)
+            {
+                result.Add(-nums[i] * -nums[i]);
+                i--;
+            }
+
+            while (j <= nums.Length - 1)
+            {
+                result.Add(nums[j] * nums[j]);
+                j++;
+            }
+
+            return result.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[3] { -1, 2, 2 };
+            int[] arr2 = new int[9] { -7, 2, 3, 4, 11, 12, 16, 25, 50 };
+
+            SortedSquares(arr1);
+        }
+    }
+}
