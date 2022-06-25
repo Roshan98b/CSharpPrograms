5c983929637ef64bdfefda92a9df889457deaf9d
Roshan Badrinath <35145161+Roshan98b@users.noreply.github.com>
2022-06-25 17:44:36 +0530

    Added BST validation method (#6)

---
+++ ValidateBST/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Collection;
+
+namespace ValidateBST
+{
+    internal class Program
+    {
+        private static readonly BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+        static void Main(string[] args)
+        {
+            bst.Insert(2);
+            bst.Insert(1);
+            bst.Insert(3);
+
+            Console.WriteLine($"Is Valid BST : {bst.IsValidBST()}");
+        }
+    }
+}
