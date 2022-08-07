febd98d543a0749ded1676f74cbce080fd2096e1
Roshan Badrinath <robadrin@microsoft.com>
2022-08-07 10:26:46 +0530

    Updated permutation computation

--- Problems/Permutations/Program.cs
+++ Problems/Permutations/Program.cs
@@ -6,7 +6,7 @@ namespace Permutations
     {
         static void Main(string[] args)
         {
-            var permutations = Permute(new int[] { 1, 2, 3 });
+            var permutations = Permute(new int[] { 2, 3, 3, 2 });
             Console.WriteLine("Permutations are: ");
             Console.WriteLine(JsonConvert.SerializeObject(permutations));
         }        
@@ -27,11 +27,40 @@ namespace Permutations
                 var target = new List<List<int>>();
                 foreach (var num in nums)
                 {
-                    var result = Permute(nums.Where(x => x != num).ToList());
+                    // Removing the number
+                    var slicedNum = new List<int>(nums);
+                    var pos = 0;
+                    for (int i = 0; i < slicedNum.Count; i++)
+                    {
+                        if (slicedNum[i] == num)
+                        {
+                            pos = i;
+                            break; // Repeating numbers should be deleted only once
+                        }
+                    }
+                    slicedNum.RemoveAt(pos);
+
+                    var result = Permute(slicedNum);
                     target.AddRange( result.Select(x => { var y = new List<int> { num };  y.AddRange(x); return y; }) );
                 }
                 return target;
             }
         }
+
+        private static bool IsValidTime(List<int> num)
+        {
+            int hours = num[0] * 10 + num[1];
+            int minutes = num[2] * 10 + num[3];
+
+            if (hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
