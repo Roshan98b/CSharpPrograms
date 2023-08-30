0635f442e56b896654e10857e6074e7f1cc211b7
Roshan Badrinath <35145161+Roshan98b@users.noreply.github.com>
2023-08-30 21:32:17 +0530

    Add linked list and swap program (#13)

---
+++ Problems/LinkedListSwapNodes/Program.cs
@@ -0,0 +1,49 @@
+﻿using Collection;
+using Collection.Models;
+
+namespace LinkedListSwapNodes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListNode head = LinkedList.CreateLinkedListFromArray(new int[] { 1, 2, 3, 4, 5 });
+            LinkedList.DisplayLinkedList(head);
+            SwapNodes(head, 2);
+            LinkedList.DisplayLinkedList(head);
+        }
+
+        public static ListNode SwapNodes(ListNode head, int k)
+        {
+            ListNode kNode = null;
+            int count = 0;
+
+            for (var curr = head; curr != null; curr = curr.next)
+            {
+                count++;
+
+                if (count == k)
+                {
+                    kNode = curr;
+                }
+            }
+
+            int lastK = (count + 1) - k;
+            count = 0;
+
+            for (var curr = head; curr != null; curr = curr.next)
+            {
+                count++;
+
+                if (count == lastK)
+                {
+                    var temp = kNode.val;
+                    kNode.val = curr.val;
+                    curr.val = temp;
+                }
+            }
+
+            return head;
+        }
+    }
+}
