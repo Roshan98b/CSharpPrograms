520d048b1cbf17a08d21e2376d316b4392f85f38
Roshan Badrinath <35145161+Roshan98b@users.noreply.github.com>
2023-12-29 10:15:41 +0530

    Update 75 (#19)
    * Reverse Vowels
    
    * Update

---
+++ Problems/LongestOneSubArray/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LongestOneSubArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().LongestSubarray(new int[] { 1, 1, 0, 0, 1, 1, 1, 0, 1 }));
+        }
+
+        public int LongestSubarray(int[] nums)
+        {
+            int i = 0;
+            while (i < nums.Length && nums[i] != 1) i++;
+
+            int j = i;
+            int max = 0, zero = 0;
+            while (j < nums.Length && zero < 2)
+            {
+                if (nums[j] == 0) zero++;
+                if (nums[j] == 1) max++;
+                j++;
+            }
+
+            if (zero == 0) return max - 1;
+            else if (zero == 1) return max;
+            else
+            {
+                int curr = max;
+                i++;
+                while (j < nums.Length)
+                {
+                    if (nums[i - 1] == 1) curr--;
+                    if (nums[j] == 1) curr++;
+                    if (curr < 1) curr = 0;
+                    if (curr > max)
+                    {
+                        max = curr;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    j++;
+                }
+
+                return max;
+            }
+        }
+    }
+}
---
+++ Problems/ReverseVowels/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace ReverseVowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().ReverseVowels("Hello, World!"));
+        }
+
+        public string ReverseVowels(string s)
+        {
+            var vowels = new List<char>() { 'a', 'A', 'E', 'e', 'i', 'I', 'O', 'o', 'u', 'U' };
+            var reversedString = new StringBuilder(s);
+
+            int i = 0, j = s.Length - 1;
+            while (i < j) 
+            {
+                bool isFirstVowel = vowels.Contains(reversedString[i]);
+                bool isLastVowel = vowels.Contains(reversedString[j]);
+
+                if (!isFirstVowel) i++;
+                if (!isLastVowel) j--;
+
+                if (isFirstVowel && isLastVowel)
+                {
+                    char temp = reversedString[i];
+                    reversedString[i] = reversedString[j];
+                    reversedString[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            return reversedString.ToString();
+        }
+    }
+}
+
+
